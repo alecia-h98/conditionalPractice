25e1d4dc2d6d3205119def141584fae331fe673c
Alecia Hagen <alecia.h98@gmail.com>
2024-11-16 12:48:53 -0600

    added passenger information

---
+++ conditional.js
@@ -0,0 +1,11 @@
+// the road trip planner
+let seatsInCar = 4;
+let passengers = 2;
+
+//check that there's enough room for everyone
+if( seatsInCar >= passengers ){
+console.log( 'ready for road trip' );
+} //end enough seats
+else{
+console.log( 'not ready. check car, passengers, and equipment' );
+} // end not enough seats

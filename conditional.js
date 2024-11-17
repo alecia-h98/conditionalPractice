d108c6132b2bf5d274233b207ce1fc6b1cab30d6
Alecia Hagen <alecia.h98@gmail.com>
2024-11-16 19:47:04 -0600

    finial results

--- conditional.js
+++ conditional.js
@@ -1,12 +1,19 @@
 // the road trip planner
 let seatsInCar = 7;
 let passengers = 5;
-let infants = 0;
-let infantSeats = 0;
+let infants = 1;
+let infantSeats = 1;
+let fullTank = true;
 //check that there's enough room for everyone
-if( seatsInCar >= passengers ){
+if( seatsInCar >= passengers && infantSeats >= infants ){
 console.log( 'ready for road trip' );
-} //end enough seats
+if( fullTank ){
+    console.log( 'ready to hit the highway' );
+} //end fullTank
+else{
+    console.log( 'must gas up before on Interstate' );
+}
+} //end enough seats & gas
 else{
 console.log( 'not ready. check car, passengers, and equipment' );
 } // end not enough seats
